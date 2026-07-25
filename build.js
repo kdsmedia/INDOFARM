76446c448e54015cec6168426a4a40cb53e0b6a2
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2026-07-25 16:40:33 +0700

    Delete build.js

--- build.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { existsSync } from 'node:fs';
-
-if (!existsSync('game/index.html')) {
-  throw new Error('Build input missing: game/index.html');
-}
-
-console.log('INDOFARM native Android bundle is ready for Capacitor.');
